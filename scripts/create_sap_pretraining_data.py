6274325457d6bd688e4568e2274ef7737c796a82
Bram van Es <bramiozo@gmail.com>
2025-01-21 14:27:35 +0100

    looking at .tsv for multilabels

---
+++ scripts/create_sap_pretraining_data.py
@@ -0,0 +1,9 @@
+"""
+    Based on UMLS relationships extract a synonym list
+"""
+
+import dotenv
+import pandas
+import json
+import re
+import os
---
+++ scripts/extend_pretraining_data.py
@@ -0,0 +1,10 @@
+"""
+    Given the prior synonyms, extend the synonyms using medical embeddings 
+    and LLMs
+"""
+
+import dotenv
+import pandas
+import json
+import re
+import os
