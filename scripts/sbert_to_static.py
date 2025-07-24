42e9969cf345edf507e8a615c3b574ced9d25faa
Bram <bramiozo@gmail.com>
2025-07-24 19:35:19 +0200

    working model2vec

--- scripts/sbert_to_static.py
+++ scripts/sbert_to_static.py
@@ -21,23 +21,25 @@ def main():
     parser.add_argument("--split", required=False, help="Which split of the dataset to be used for the featurization", default="train")
     parser.add_argument("--device", required=False, choices=["cpu", "cuda"], default="cpu")
     parser.add_argument("--preptype", choices=["train", "distill"], default="distill")
-    parser.add_argument("--pca_dims", type=int, default=256)
+    parser.add_argument("--featurize", action="store_true")
+    parser.add_argument("--pca_dims", type=int, default=384)
     args = parser.parse_args()
 
 
-    model = SentenceTransformer(args.model)
+    st_model = SentenceTransformer(args.model)
     output_dir = "output"
 
     if args.preptype=='train':
-        my_corpus = load_dataset(args.dataset, split="train", streaming=True, token=TOKEN)
-        featurize(
-            dataset=my_corpus,
-            model=model,
-            output_dir=output_dir,
-            max_means=2_000_000,
-            batch_size=128,
-            text_key="text"
-        )
+        if args.featurize:
+            my_corpus = load_dataset(args.dataset, split="train", streaming=True, token=TOKEN)
+            featurize(
+                dataset=my_corpus,
+                model=st_model,
+                output_dir=output_dir,
+                max_means=2_000_000,
+                batch_size=128,
+                text_key="text"
+            )
 
         data_dir = output_dir
 
@@ -45,8 +47,10 @@ def main():
         paths = sorted(Path(data_dir).glob("*.json"))
         train_txt, train_vec = collect_means_and_texts(paths)
 
+        static_model_init = distill(model_name=args.model, pca_dims=args.pca_dims, trust_remote_code=False)
+
         model = train_model(
-            args.model,
+            static_model_init,
             train_txt,
             train_vec,
             device=args.device,
