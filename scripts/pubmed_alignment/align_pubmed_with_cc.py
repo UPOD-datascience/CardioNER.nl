85c5eec38c861171ddab62012072ec66cbc00d67
Bram van Es <bramiozo@gmail.com>
2025-12-29 18:02:35 +0100

    ...

---
+++ scripts/pubmed_alignment/align_pubmed_with_cc.py
@@ -0,0 +1,14 @@
+# - given a pubmed dataset (HF, Parquet, or jsonl)
+# - given a csv file with pmid, copyright statements (CC BY.. etc)
+# - output the pubmed dataset with cc columns (CC_type, requires_attribution, modify_or_translate, can_use_commercially, share_w_same_license)
+#
+def map_cc_to_statements():
+    # cc --> requires_attribution, modify_or_translate, can_use_commercially, share_w_same_license
+    pass
+
+def load_pmid_cc():
+    #
+    pass
+
+def load_hf_dataset():
+    pass
